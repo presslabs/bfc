214ef692885f3c150fa007c8b8c966ac1bbf4714
Calin Don <calin.don@gmail.com>
2020-12-07 15:41:45 +0200

    Sync setup-credentials-helper.sh with github.com/presslabs/build
    * Add support for docker login
    * Renamed env var PROJECT to GOOGLE_CLOUD_PROJECT
    * Remove tiller init

--- utils/setup-credentials-helper.sh
+++ utils/setup-credentials-helper.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 : ${GOOGLE_CREDENTIALS:="$(cat "$PLUGIN_GOOGLE_CREDENTIALS_FILE" 2>/dev/null)"}
-: ${PROJECT:="$PLUGIN_PROJECT"}
+: ${GOOGLE_CLOUD_PROJECT:="$PLUGIN_GOOGLE_CLOUD_PROJECT"}
 : ${CLUSTER:="$PLUGIN_CLUSTER"}
 : ${ZONE:="$PLUGIN_ZONE"}
 : ${UPGRADE_TILLER:="$PLUGIN_UPGRADE_TILLER"}
 : ${SSH_KEY:="$PLUGIN_SSH_KEY"}
+: ${DOCKER_USERNAME:="$PLUGIN_DOCKER_USERNAME"}
+: ${DOCKER_PASSWORD:="$PLUGIN_DOCKER_PASSWORD"}
+: ${DOCKER_REGISTRY:="${PLUGIN_DOCKER_REGISTRY:-docker.io}"}
+
+export PATH="$CI_WORKSPACE/bin:$PATH"
 
 require_param() {
     declare name="$1"
@@ -28,13 +33,20 @@ run() {
     "$@"
 }
 
-if [ ! -z "$GOOGLE_CREDENTIALS" ] ; then
-    echo "$GOOGLE_CREDENTIALS" > /run/google-credentials.json
-    run gcloud auth activate-service-account --key-file=/run/google-credentials.json
+if [ ! -z "$DOCKER_PASSWORD" ] ; then
+    require_param DOCKER_USERNAME
+    echo "+ docker login $DOCKER_REGISTRY -u $DOCKER_USERNAME"
+    docker login $DOCKER_REGISTRY -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 fi
 
-if [ ! -z "$PROJECT" ] ; then
-    run gcloud config set project "$PROJECT"
+if [ ! -z "$GOOGLE_CREDENTIALS" ] ; then
+    echo "$GOOGLE_CREDENTIALS" > /run/google-credentials.json
+    run gcloud auth activate-service-account --quiet --key-file=/run/google-credentials.json
+    run gcloud auth configure-docker --quiet
+fi
+
+if [ ! -z "$GOOGLE_CLOUD_PROJECT" ] ; then
+    run gcloud config set project "$GOOGLE_CLOUD_PROJECT"
 fi
 
 if [ ! -z "$CLUSTER" ] ; then
@@ -43,14 +55,9 @@ if [ ! -z "$CLUSTER" ] ; then
     require_param "project"
     require_param "zone"
 
-    run gcloud container clusters get-credentials "$CLUSTER" --project "$PROJECT" --zone "$ZONE"
+    run gcloud container clusters get-credentials "$CLUSTER" --project "$GOOGLE_CLOUD_PROJECT" --zone "$ZONE"
     # Display kubernetees versions (usefull for debugging)
     run kubectl version
-
-    # initialize helm
-    if [ "$UPGRADE_TILLER" == "true" ] || [ "$UPGRADE_TILLER" == "yes" ]; then
-        run helm init --upgrade
-    fi
 fi
 
 if [ ! -z "$SSH_KEY" ] ; then
